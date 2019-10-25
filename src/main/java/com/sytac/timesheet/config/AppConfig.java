611548e6e998cecd2810d89a2305addc8724b7be
Khalid Gaber <khalid.gaber@ing.com>
2019-10-25 17:03:52 +0200

    added rest template bean

--- src/main/java/com/sytac/timesheet/config/AppConfig.java
+++ src/main/java/com/sytac/timesheet/config/AppConfig.java
@@ -3,6 +3,7 @@ package com.sytac.timesheet.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
@@ -10,6 +11,11 @@ public class AppConfig {
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper().findAndRegisterModules();
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
     }
 
 }
