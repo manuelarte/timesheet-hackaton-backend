a6ba065b57e3bd95fd535ce7e9f5d505374124d8
Khalid Gaber <khalid.gaber@ing.com>
2019-10-25 17:49:58 +0200

    Added authentication and accepting json as media type for bamboo client

--- src/main/java/com/sytac/timesheet/config/AppConfig.java
+++ src/main/java/com/sytac/timesheet/config/AppConfig.java
@@ -1,9 +1,14 @@
 package com.sytac.timesheet.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import static org.springframework.http.HttpHeaders.ACCEPT;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Configuration
 public class AppConfig {
@@ -14,8 +19,14 @@ public class AppConfig {
     }
 
     @Bean
-    public RestTemplate getRestTemplate() {
-        return new RestTemplate();
+    public RestTemplate getRestTemplate(
+            RestTemplateBuilder restTemplateBuilder,
+            @Value("${bamboo.api.username}") String username,
+            @Value("${bamboo.api.password}") String password) {
+        return restTemplateBuilder
+                .basicAuthentication(username, password)
+                .defaultHeader(ACCEPT, APPLICATION_JSON_VALUE)
+                .build();
     }
 
 }
