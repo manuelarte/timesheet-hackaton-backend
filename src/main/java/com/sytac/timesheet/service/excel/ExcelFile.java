71718e639ce90481ef9d3e7e220c7499219616ce
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2019-10-25 19:10:24 +0200

    Changing to double

--- src/main/java/com/sytac/timesheet/service/excel/ExcelFile.java
+++ src/main/java/com/sytac/timesheet/service/excel/ExcelFile.java
@@ -46,7 +46,7 @@ public class ExcelFile {
         }
     }
 
-    public void addHours(final int day, final Integer hours, final HoursType type) {
+    public void addHours(final int day, final Double hours, final HoursType type) {
         if (hours == null || hours <= 0) {
             return;
         }
